abfd2d4db0cac39cb542c9baaa3e325afe9b3681
shshgaga <y.matsumoto.backs@gmail.com>
2024-06-27 13:59:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
-from selenium import webdriver
-from tempfile import mkdtemp
-from selenium.webdriver.common.by import By
+import boto3
+import pickle
+import pandas as pd
 
+def load_pickle_from_s3(bucket_name, file_key):
+    s3 = boto3.client('s3')
+    try:
+        response = s3.get_object(Bucket=bucket_name, Key=file_key)
+        body = response['Body'].read()
+        return pickle.loads(body)
+    except Exception as e:
+        print(f"Error reading {file_key} from bucket {bucket_name}: {e}")
+        return None
 
-def handler(event=None, context=None):
-    options = webdriver.ChromeOptions()
-    service = webdriver.ChromeService("/opt/chromedriver")
-
-    options.binary_location = '/opt/chrome/chrome'
-    options.add_argument("--headless=new")
-    options.add_argument('--no-sandbox')
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1280x1696")
-    options.add_argument("--single-process")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-dev-tools")
-    options.add_argument("--no-zygote")
-    options.add_argument(f"--user-data-dir={mkdtemp()}")
-    options.add_argument(f"--data-path={mkdtemp()}")
-    options.add_argument(f"--disk-cache-dir={mkdtemp()}")
-    options.add_argument("--remote-debugging-port=9222")
-
-    chrome = webdriver.Chrome(options=options, service=service)
-    chrome.get("https://example.com/")
-
-    return chrome.find_element(by=By.XPATH, value="//html").text
+def lambda_handler(event, context):
+    bucket_name = 'layerk'  # S3バケット名
+    file_key = '2024aws.pkl'  # S3のファイルキー
+    
+    # S3からファイルを読み込んでデシリアライズ
+    df_moto = load_pickle_from_s3(bucket_name, file_key)
+    
+    if df_moto is not None:
+        print(f"Successfully loaded DataFrame with shape: {df_moto.shape}")
+        return {
+            'statusCode': 200,
+            'body': 'Successfully loaded pickle file from S3.'
+        }
+    else:
+        return {
+            'statusCode': 500,
+            'body': 'Failed to load pickle file from S3.'
+        }
