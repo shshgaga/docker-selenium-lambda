a025e489f7172b21b5bb0daca42da07386e618e7
shshgaga <y.matsumoto.backs@gmail.com>
2024-06-28 16:51:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
-from datetime import datetime, timedelta
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.alert import Alert
-from selenium.webdriver.chrome import service as fs
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -16,7 +14,7 @@ import re
 import boto3
 import io
 from tempfile import mkdtemp
-
+import json
 
 def load_pickle_from_s3(bucket_name, file_key):
     s3 = boto3.client('s3')
@@ -28,12 +26,11 @@ def load_pickle_from_s3(bucket_name, file_key):
         print(f"Error reading {file_key} from bucket {bucket_name}: {e}")
         return None
 
-
 # 処理を行う関数
 def lambda_handler(event, context):
-    options = webdriver.ChromeOptions()
-    service = webdriver.ChromeService("/opt/chromedriver")
-
+    options = Options()
+    service = ChromeService("/opt/chromedriver")
+    
     options.binary_location = '/opt/chrome/chrome'
     options.add_argument("--headless=new")
     options.add_argument('--no-sandbox')
@@ -47,16 +44,19 @@ def lambda_handler(event, context):
     options.add_argument(f"--data-path={mkdtemp()}")
     options.add_argument(f"--disk-cache-dir={mkdtemp()}")
     options.add_argument("--remote-debugging-port=9222")
-    driver = webdriver.Chrome(options=options, service=service)
-
-    bucket_name="layerk"
-    file_key="ol.pkl"
     
-    ol=[]
-    id_list=load_pickle_from_s3(bucket_name, file_key)
+    driver = webdriver.Chrome(service=service, options=options)
+    wait = WebDriverWait(driver, 10)  # WebDriverWaitのインスタンスを初期化
+
+    bucket_name = "layerk"
+    file_key = "ol.pkl"
+    
+    ol = []
+    id_list = load_pickle_from_s3(bucket_name, file_key)
     base_url = 'https://race.netkeiba.com/odds/index.html'
+    
     for id in id_list:
-        param_race_id = str(id)[:4]+str(id)[-8:]
+        param_race_id = str(id)[:4] + str(id)[-8:]
         url = f'{base_url}?type=b8&race_id={param_race_id}&housiki=C2'
         try:
             driver.get(url)
@@ -75,17 +75,23 @@ def lambda_handler(event, context):
             print(f"その他のエラーが発生しました。race_id: {id} - {str(e)}")
         finally:
             driver.quit()
-            driver = webdriver.Chrome(options=options, service=service)
-    do=pd.concat(ol)
+            driver = webdriver.Chrome(service=service, options=options)
+
+    # driver.quit()はforループの外で呼び出す
+    driver.quit()
+    
+    do = pd.concat([df for df_list in ol for df in df_list])
     # DataFrameをpickle形式でバイナリデータに変換
     pickle_buffer = io.BytesIO()
     do.to_pickle(pickle_buffer)
     pickle_buffer.seek(0)
+    
     # S3バケットにpickleファイルをアップロード
     s3 = boto3.client('s3')
     bucket_name = 'layerk'
     object_name = 'file.pkl'
     s3.put_object(Bucket=bucket_name, Key=object_name, Body=pickle_buffer)
+    
     return {
         'statusCode': 200,
         'body': json.dumps('DataFrame has been successfully saved to S3 as a pickle file.')
